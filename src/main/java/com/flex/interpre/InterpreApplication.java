449271d86aa08b83a16c4deeb1d84c8ddc69c102
kimmoongi <moongi0320@naver.com>
2025-09-22 00:14:30 +0900

    [IPR-1] feat(springboot): initial commit

---
+++ src/main/java/com/flex/interpre/InterpreApplication.java
@@ -0,0 +1,13 @@
+package com.flex.interpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterpreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterpreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flex/interpre/InterpreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flex.interpre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterpreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
