97b0c0d6e532651baa0a88cb99324dad0bda7773
docherryra <docherryra@gmail.com>
2025-09-24 18:15:16 +0900

    [IPR-11] feat(user): 구직자, 기업 엔티티 구현

--- src/main/java/com/flex/interpre/domain/user/entity/Company.java
+++ src/main/java/com/flex/interpre/domain/user/entity/Company.java
@@ -1,4 +1,40 @@
 package com.flex.interpre.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.UUID;
+
+@Entity
+@Getter @Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED) @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "companies")
 public class Company {
+    @Id
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name="company_name", nullable = false, length = 200)
+    private String companyName;
+
+    @Column(name="business_number", nullable = false, length = 50, unique = true)
+    private String businessNumber;
+
+    @Column(columnDefinition = "TEXT")
+    private String address;
+
+    @Column(length = 255)
+    private String website;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "logo_url", length = 500)
+    private String logoUrl;
 }
--- src/main/java/com/flex/interpre/domain/user/entity/JobSeeker.java
+++ src/main/java/com/flex/interpre/domain/user/entity/JobSeeker.java
@@ -1,4 +1,43 @@
 package com.flex.interpre.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter @Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED) @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "job_seekers")
 public class JobSeeker {
+    @Id
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private Education education;
+
+    @ElementCollection
+    @CollectionTable(name = "job_seeker_desired_areas",
+            joinColumns = @JoinColumn(name = "job_seeker_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "desired_area")
+    private List<Area> desiredAreas;
+
+    @ElementCollection
+    @CollectionTable(name = "job_seeker_desired_categories",
+            joinColumns = @JoinColumn(name = "job_seeker_id"))
+    @Column(name = "desired_job_category")
+    private List<JobCategoty> desiredJobCategories;
 }
