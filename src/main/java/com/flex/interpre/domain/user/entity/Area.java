57689ca126f828e51402078d9036c1f2cdb47aaa
docherryra <docherryra@gmail.com>
2025-09-24 18:14:58 +0900

    [IPR-11] feat(user): 필요 enum 클래스 추가

---
+++ src/main/java/com/flex/interpre/domain/user/entity/Area.java
@@ -0,0 +1,8 @@
+package com.flex.interpre.domain.user.entity;
+
+public enum Area {
+    서울,
+    경기도,
+    인천
+    //TODO: 지역 값 정의 후 enum 정의
+}
---
+++ src/main/java/com/flex/interpre/domain/user/entity/Education.java
@@ -0,0 +1,11 @@
+package com.flex.interpre.domain.user.entity;
+
+public enum Education {
+    초등학교_졸업,
+    중학교_졸업,
+    고등학교_졸업,
+    대학교_2년제,
+    대학교_3년제,
+    대학교_4년제,
+    해당사항없음
+}
