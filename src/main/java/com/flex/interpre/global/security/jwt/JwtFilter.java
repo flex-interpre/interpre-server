da3d08e020ccb9436eb2238ec2b73c1467111d5a
kimmoongi <moongi0320@naver.com>
2025-09-24 20:09:48 +0900

    [IPR-9] feat(jwtFilter): filter에 authentication 추가 및 예외처리 추가

--- src/main/java/com/flex/interpre/global/security/jwt/JwtFilter.java
+++ src/main/java/com/flex/interpre/global/security/jwt/JwtFilter.java
@@ -1,11 +1,19 @@
 package com.flex.interpre.global.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flex.interpre.domain.user.entity.User;
+import com.flex.interpre.domain.user.exception.UserExceptions;
+import com.flex.interpre.domain.user.repository.UserRepository;
+import com.flex.interpre.global.dto.ApiResponse;
+import com.flex.interpre.global.exception.ApiException;
+import com.flex.interpre.global.security.authentication.UserAuthentication;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +25,8 @@ import java.util.UUID;
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(
@@ -30,11 +40,20 @@ public class JwtFilter extends OncePerRequestFilter {
             if (jwtUtil.validateToken(accessToken)){
                 UUID id = jwtUtil.extractUUID(accessToken);
 
-                //TODO: id로 repo에서 User 객체 가져와서 authentication 생성
-            }
-        }catch (Exception e){
+                User user = userRepository.findById(id).orElseThrow(UserExceptions.USER_NOT_FOUND::toException);
 
-            // TODO: ApiException 처리 코드 생성
+                UserAuthentication authentication = new UserAuthentication(user);
+                authentication.setAuthenticated(true);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }catch (ApiException e){
+
+            ApiResponse<?> apiResponse = ApiResponse.error(e);
+
+            String body = objectMapper.writeValueAsString(apiResponse);
+            response.setStatus(e.getHttpStatus().value());
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(body);
             return;
         }
 
