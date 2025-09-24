843c1b9ed6c46422f198afe238a4a34b9480867c
kimmoongi <moongi0320@naver.com>
2025-09-24 20:08:22 +0900

    [IPR-9] feat(authentication): 유저 객체 저장하는 커스텀 authentication 생성

---
+++ src/main/java/com/flex/interpre/global/security/authentication/UserAuthentication.java
@@ -0,0 +1,47 @@
+package com.flex.interpre.global.security.authentication;
+
+import com.flex.interpre.domain.user.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserAuthentication implements Authentication {
+
+    private final User user;
+
+    @Getter
+    @Setter
+    private boolean authenticated;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return user;
+    }
+
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
