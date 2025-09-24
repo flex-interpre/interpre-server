86eac295ae7abedead9251b5f2349dbb735a250d
IyeonHan <104899885+docherryra@users.noreply.github.com>
2025-09-24 09:26:51 +0900

    [0.1.0] IPR-2 swagger설정 및 공통 ApiResponse 구현 (#1)
    * [IPR-2] feat: swagger 의존성 추가
    
    * [IPR-2] feat(swagger): swagger config 클래스 추가
    
    * [IPR-2] feat(exception): 공통 응답 구조 구현

---
+++ src/main/java/com/flex/interpre/global/exception/ApiException.java
@@ -0,0 +1,15 @@
+package com.flex.interpre.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiException extends RuntimeException {
+    private final HttpStatus httpStatus;
+    public ApiException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
