95eac180358a2508a0bda9aa6541eadfc4926841
docherryra <docherryra@gmail.com>
2025-09-25 12:52:37 +0900

    [IPR-11] feat(user): 근무지역 enum 값 추가

--- src/main/java/com/flex/interpre/domain/user/entity/Area.java
+++ src/main/java/com/flex/interpre/domain/user/entity/Area.java
@@ -1,8 +1,6 @@
 package com.flex.interpre.domain.user.entity;
 
 public enum Area {
-    서울,
-    경기도,
-    인천
-    //TODO: 지역 값 정의 후 enum 정의
+    서울, 경기, 인천, 부산, 대구, 광주, 대전, 울산,
+    세종, 강원, 경남, 경북, 전남, 전북, 충남, 충북, 제주
 }
