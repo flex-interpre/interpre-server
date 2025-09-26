0c272eea7fab57fcbd93e2ab3e24224b013c5abe
kimmoongi <moongi0320@naver.com>
2025-09-26 15:36:26 +0900

    [IPR-3] feat(auth): 로그인 하면 accessToken, refreshToken 쿠키에 넣어 전달

--- src/main/java/com/flex/interpre/domain/auth/handler/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/flex/interpre/domain/auth/handler/CustomOAuth2SuccessHandler.java
@@ -2,6 +2,8 @@ package com.flex.interpre.domain.auth.handler;
 
 import com.flex.interpre.domain.user.entity.User;
 import com.flex.interpre.domain.user.repository.UserRepository;
+import com.flex.interpre.global.security.jwt.JwtUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Component
@@ -17,9 +20,10 @@ import java.util.Map;
 public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
@@ -31,7 +35,14 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
 
         User user = userRepository.findByGoogleId((String) oAuth2User.getAttributes().get("sub")).orElseGet(() -> from(oAuth2User.getAttributes(),role));
 
+        String accessToken =  jwtUtil.generateToken(user);
+        String refreshToken = jwtUtil.generateRefreshToken(user);
 
+        setCookie(accessToken,refreshToken,response);
+
+        response.sendRedirect("http://localhost:3000/callback");
+
+        
     }
 
     private User from(Map<String, Object> attributes, User.Role role) {
@@ -47,5 +58,22 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
                         .approved(approved)
                         .build()
         );
+    }
+
+    private void setCookie(String accessToken, String refreshToken, HttpServletResponse response) {
+
+        addTokenCookie(response, "accessToken", accessToken);
+        addTokenCookie(response, "refreshToken", refreshToken);
+    }
+
+    private void addTokenCookie(HttpServletResponse response, String name, String value) {
+
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(false);
+        cookie.setSecure(false);
+        cookie.setMaxAge(300); // 짧게 5분
+
+        response.addCookie(cookie);
     }
 }
