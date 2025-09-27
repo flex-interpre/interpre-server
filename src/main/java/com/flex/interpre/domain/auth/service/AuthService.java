8fed8ee091698a4a69b6b2482bf0e447ad128b04
kimmoongi <moongi0320@naver.com>
2025-09-27 11:03:41 +0900

    [IPR-4] feat(auth): 로그아웃 api 요청 추가

---
+++ src/main/java/com/flex/interpre/domain/auth/service/AuthService.java
@@ -0,0 +1,31 @@
+package com.flex.interpre.domain.auth.service;
+
+import com.flex.interpre.domain.auth.entity.RefreshToken;
+import com.flex.interpre.domain.auth.exception.AuthExceptions;
+import com.flex.interpre.domain.auth.repository.RefreshTokenRespository;
+import com.flex.interpre.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final RefreshTokenRespository refreshTokenRespository;
+
+    @Transactional
+    @PreAuthorize("isAuthenticated()")
+    public void logout(User user, String token){
+
+        RefreshToken refreshToken = refreshTokenRespository.findByRefreshToken(token)
+                .orElseThrow(AuthExceptions.INVALID_TOKEN::toException);
+
+        if (!refreshToken.getUserId().equals(user.getId().toString())) {
+            throw AuthExceptions.INVALID_TOKEN.toException();
+        }
+
+        refreshTokenRespository.delete(refreshToken);
+    }
+}
