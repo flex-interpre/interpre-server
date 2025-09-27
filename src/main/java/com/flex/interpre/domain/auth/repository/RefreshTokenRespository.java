0433b2589d430fc5066abfbe351da07eb59e230b
kimmoongi <moongi0320@naver.com>
2025-09-27 11:03:15 +0900

    [IPR-4] feat(refresh): refresh token으로 redis에서 RefreshToken 찾는 메소드 추가

--- src/main/java/com/flex/interpre/domain/auth/repository/RefreshTokenRespository.java
+++ src/main/java/com/flex/interpre/domain/auth/repository/RefreshTokenRespository.java
@@ -3,5 +3,9 @@ package com.flex.interpre.domain.auth.repository;
 import com.flex.interpre.domain.auth.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRespository extends CrudRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
