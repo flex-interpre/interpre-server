dd83d786dae9082ad2257d29f27486adb5fc4310
kimmoongi <moongi0320@naver.com>
2025-09-28 15:04:38 +0900

    [IPR-5] feat(auth): 토큰 재발급 api 생성

--- src/main/java/com/flex/interpre/domain/auth/controller/AuthController.java
+++ src/main/java/com/flex/interpre/domain/auth/controller/AuthController.java
@@ -1,16 +1,15 @@
 package com.flex.interpre.domain.auth.controller;
 
 import com.flex.interpre.domain.auth.dto.request.RefreshTokenRequest;
+import com.flex.interpre.domain.auth.dto.response.TokenResponse;
 import com.flex.interpre.domain.auth.service.AuthService;
 import com.flex.interpre.domain.user.entity.User;
 import com.flex.interpre.global.dto.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +24,11 @@ public class AuthController {
         authService.logout(user, request.getRefreshToken());
 
         return ApiResponse.ok();
+    }
+
+    @PatchMapping("/sessions")
+    public ApiResponse<TokenResponse> regenerateToken(HttpServletRequest request, @Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+
+        return ApiResponse.ok(authService.regenerateToken(request, refreshTokenRequest.getRefreshToken()));
     }
 }
