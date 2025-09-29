7307b13857dd7f3b29882c74330f8c6ecaee862b
docherryra <docherryra@gmail.com>
2025-09-29 13:24:07 +0900

    [IPR-17] feat(user): dto 추가, UserUpdateRequest로 요청 받고 역할 필드로 구직자/기업 분기

--- src/main/java/com/flex/interpre/domain/user/dto/request/UpdateMyCompanyInfo.java
+++ src/main/java/com/flex/interpre/domain/user/dto/request/UpdateMyCompanyInfo.java
@@ -1,4 +1,20 @@
 package com.flex.interpre.domain.user.dto.request;
 
-public record UpdateMyCompanyInfo() {
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flex.interpre.domain.user.entity.Role;
+
+public record UpdateMyCompanyInfo(
+        String companyName,
+        String businessNumber,
+        String address,
+        String website,
+        String description,
+        String logoUrl
+) implements UserUpdateRequest {
+
+    @JsonIgnore
+    public Role getRole() {
+        return Role.COMPANY;
+    }
 }
