75edc0644c1cee576ebc43002360d371e2437615
docherryra <docherryra@gmail.com>
2025-09-29 13:24:25 +0900

    [IPR-17] feat(user): 구직자/기업 정보 수정 API 구현

--- src/main/java/com/flex/interpre/domain/user/controller/UserController.java
+++ src/main/java/com/flex/interpre/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.flex.interpre.domain.user.controller;
 
 
+import com.flex.interpre.domain.user.dto.request.UserUpdateRequest;
 import com.flex.interpre.domain.user.dto.response.MyUserDetailInfo;
 import com.flex.interpre.domain.user.entity.User;
 import com.flex.interpre.domain.user.service.UserService;
@@ -8,9 +9,7 @@ import com.flex.interpre.global.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -22,5 +21,12 @@ public class UserController {
     @Operation(summary = "내 정보 조회 - Role-> 구직자와 기업 분기")
     public ApiResponse<MyUserDetailInfo> getUserInfo(@AuthenticationPrincipal User user){
         return ApiResponse.ok(userService.getUserInfo(user));
+    }
+
+    @PutMapping("/me")
+    @Operation(summary = "내 정보 수정")
+    public ApiResponse<Void> updateUserInfo(@AuthenticationPrincipal User user, @RequestBody UserUpdateRequest request){
+        userService.updateUserInfo(user, request);
+        return ApiResponse.ok();
     }
 }
--- src/main/java/com/flex/interpre/domain/user/service/UserService.java
+++ src/main/java/com/flex/interpre/domain/user/service/UserService.java
@@ -1,11 +1,13 @@
 package com.flex.interpre.domain.user.service;
 
+import com.flex.interpre.domain.user.dto.request.UpdateMyCompanyInfo;
+import com.flex.interpre.domain.user.dto.request.UpdateMyJobSeekerInfo;
+import com.flex.interpre.domain.user.dto.request.UserUpdateRequest;
 import com.flex.interpre.domain.user.dto.response.MyCompanyInfo;
 import com.flex.interpre.domain.user.dto.response.MyJobSeekerInfo;
 import com.flex.interpre.domain.user.dto.response.MyUserDetailInfo;
 import com.flex.interpre.domain.user.entity.Company;
 import com.flex.interpre.domain.user.entity.JobSeeker;
-import com.flex.interpre.domain.user.entity.Role;
 import com.flex.interpre.domain.user.entity.User;
 import com.flex.interpre.domain.user.exception.UserExceptions;
 import com.flex.interpre.domain.user.repository.CompanyRepository;
@@ -20,27 +22,53 @@ public class UserService {
     private final JobSeekerRepository jobSeekerRepository;
     private final CompanyRepository companyRepository;
 
+
+    // 유저 정보 조회
     public MyUserDetailInfo getUserInfo(User user) {
-        if  (user.getRole() == Role.JOB_SEEKER){
-            return getJobSeekerInfo(user);
-        } else if  (user.getRole() == Role.COMPANY){
-            return getCompanyInfo(user);
-        } else {
-            throw new ApiException(UserExceptions.INVALID_ROLE);
-        }
+        return switch (user.getRole()) {
+            case JOB_SEEKER -> getJobSeekerInfo(user);
+            case COMPANY -> getCompanyInfo(user);
+            default -> throw new ApiException(UserExceptions.INVALID_ROLE);
+        };
     }
 
-    public MyJobSeekerInfo getJobSeekerInfo(User user){
+    // 유저 정보 수정
+    public void updateUserInfo(User user, UserUpdateRequest request){
+        switch (request) {
+            case UpdateMyJobSeekerInfo jobSeekerInfo -> updateJobSeekerInfo(user, jobSeekerInfo);
+            case UpdateMyCompanyInfo companyInfo -> updateCompanyInfo(user, companyInfo);
+            default -> throw new ApiException(UserExceptions.INVALID_ROLE);
+        };
+    }
+
+
+    /*    내부 메서드    */
+
+    private MyJobSeekerInfo getJobSeekerInfo(User user){
         JobSeeker jobSeeker = jobSeekerRepository.findByIdWithUser(user.getId())
                 .orElseThrow(UserExceptions.USER_NOT_FOUND::toException);
 
         return MyJobSeekerInfo.from(jobSeeker);
     }
 
-    public MyCompanyInfo getCompanyInfo(User user){
+    private MyCompanyInfo getCompanyInfo(User user){
         Company company = companyRepository.findByIdWithUser(user.getId())
                 .orElseThrow(UserExceptions.USER_NOT_FOUND::toException);
 
         return MyCompanyInfo.from(company);
+    }
+
+    private void updateJobSeekerInfo(User user, UpdateMyJobSeekerInfo request){
+        JobSeeker jobSeeker = jobSeekerRepository.findByIdWithUser(user.getId())
+                .orElseThrow(UserExceptions.USER_NOT_FOUND::toException);
+
+        if (request.name() != null && !request.name().isEmpty()) {
+            jobSeeker.setName(request.name());
+        }
+    }
+
+    private void updateCompanyInfo(User user, UpdateMyCompanyInfo request){
+        Company company = companyRepository.findByIdWithUser(user.getId())
+                .orElseThrow(UserExceptions.USER_NOT_FOUND::toException);
     }
 }
