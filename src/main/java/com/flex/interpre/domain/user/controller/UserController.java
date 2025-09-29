9aee63e3c1c23ae49507ec9878a1729a1bb0df7d
docherryra <docherryra@gmail.com>
2025-09-29 10:49:49 +0900

    [IPR-7] refactor(user): 응답 dto 분기를 서비스 로직 책임으로 변경

--- src/main/java/com/flex/interpre/domain/user/controller/UserController.java
+++ src/main/java/com/flex/interpre/domain/user/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.flex.interpre.domain.user.controller;
 
-import com.flex.interpre.domain.user.dto.response.MyCompanyInfo;
-import com.flex.interpre.domain.user.dto.response.MyJobSeekerInfo;
+
 import com.flex.interpre.domain.user.dto.response.MyUserDetailInfo;
-import com.flex.interpre.domain.user.entity.Role;
 import com.flex.interpre.domain.user.entity.User;
-import com.flex.interpre.domain.user.exception.UserExceptions;
 import com.flex.interpre.domain.user.service.UserService;
 import com.flex.interpre.global.dto.ApiResponse;
-import com.flex.interpre.global.exception.ApiException;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,11 +20,7 @@ public class UserController {
 
     @GetMapping("/me")
     @Operation(summary = "내 정보 조회 - Role-> 구직자와 기업 분기")
-    public ApiResponse<MyUserDetailInfo> getJobSeekerInfo(@AuthenticationPrincipal User user){
-        return switch (user.getRole()) {
-            case JOB_SEEKER -> ApiResponse.ok(userService.getJobSeekerInfo(user));
-            case COMPANY   -> ApiResponse.ok(userService.getCompanyInfo(user));
-            default        -> throw new ApiException(UserExceptions.INVALID_ROLE);
-        };
+    public ApiResponse<MyUserDetailInfo> getUserInfo(@AuthenticationPrincipal User user){
+        return ApiResponse.ok(userService.getUserInfo(user));
     }
 }
--- src/main/java/com/flex/interpre/domain/user/service/UserService.java
+++ src/main/java/com/flex/interpre/domain/user/service/UserService.java
@@ -2,12 +2,15 @@ package com.flex.interpre.domain.user.service;
 
 import com.flex.interpre.domain.user.dto.response.MyCompanyInfo;
 import com.flex.interpre.domain.user.dto.response.MyJobSeekerInfo;
+import com.flex.interpre.domain.user.dto.response.MyUserDetailInfo;
 import com.flex.interpre.domain.user.entity.Company;
 import com.flex.interpre.domain.user.entity.JobSeeker;
+import com.flex.interpre.domain.user.entity.Role;
 import com.flex.interpre.domain.user.entity.User;
 import com.flex.interpre.domain.user.exception.UserExceptions;
 import com.flex.interpre.domain.user.repository.CompanyRepository;
 import com.flex.interpre.domain.user.repository.JobSeekerRepository;
+import com.flex.interpre.global.exception.ApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,16 @@ public class UserService {
     private final JobSeekerRepository jobSeekerRepository;
     private final CompanyRepository companyRepository;
 
+    public MyUserDetailInfo getUserInfo(User user) {
+        if  (user.getRole() == Role.JOB_SEEKER){
+            return getJobSeekerInfo(user);
+        } else if  (user.getRole() == Role.COMPANY){
+            return getCompanyInfo(user);
+        } else {
+            throw new ApiException(UserExceptions.INVALID_ROLE);
+        }
+    }
+g
     public MyJobSeekerInfo getJobSeekerInfo(User user){
         JobSeeker jobSeeker = jobSeekerRepository.findByIdWithUser(user.getId())
                 .orElseThrow(UserExceptions.USER_NOT_FOUND::toException);
