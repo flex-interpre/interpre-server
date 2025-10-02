be624ae7bf435e99c969fcd735c957dbcb23669a
docherryra <docherryra@gmail.com>
2025-10-02 19:21:41 +0900

    [IPR-17] refactor(user): 필요없는 어노테이션 제거

--- src/main/java/com/flex/interpre/domain/user/dto/request/UpdateMyCompanyInfo.java
+++ src/main/java/com/flex/interpre/domain/user/dto/request/UpdateMyCompanyInfo.java
@@ -1,8 +1,6 @@
 package com.flex.interpre.domain.user.dto.request;
 
-import jakarta.validation.Valid;
 
-@Valid
 public record UpdateMyCompanyInfo(
         String companyName,
         String businessNumber,
--- src/main/java/com/flex/interpre/domain/user/dto/request/UpdateMyJobSeekerInfo.java
+++ src/main/java/com/flex/interpre/domain/user/dto/request/UpdateMyJobSeekerInfo.java
@@ -4,11 +4,9 @@ package com.flex.interpre.domain.user.dto.request;
 import com.flex.interpre.domain.user.entity.Area;
 import com.flex.interpre.domain.user.entity.Education;
 import com.flex.interpre.domain.user.entity.JobCategoty;
-import jakarta.validation.Valid;
 
 import java.util.Set;
 
-@Valid
 public record UpdateMyJobSeekerInfo(
         String name,
         Education education,
