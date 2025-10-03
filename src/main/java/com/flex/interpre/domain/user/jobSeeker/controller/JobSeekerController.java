8095dca6d884308d436ee20eda30211bb2056048
kimmoongi <moongi0320@naver.com>
2025-10-03 22:18:20 +0900

    [IPR-14] feat(jobSeeker): 북마크 추가 api 생성

--- src/main/java/com/flex/interpre/domain/user/jobSeeker/controller/JobSeekerController.java
+++ src/main/java/com/flex/interpre/domain/user/jobSeeker/controller/JobSeekerController.java
@@ -1,5 +1,6 @@
 package com.flex.interpre.domain.user.jobSeeker.controller;
 
+import com.flex.interpre.domain.recruitment.entity.Recruitment;
 import com.flex.interpre.domain.user.entity.User;
 import com.flex.interpre.domain.user.jobSeeker.dto.resposne.BookMarkListResponse;
 import com.flex.interpre.domain.user.jobSeeker.service.JobSeekerService;
@@ -8,9 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +24,13 @@ public class JobSeekerController {
     public ApiResponse<BookMarkListResponse> getBookmarks(@AuthenticationPrincipal User user) {
 
         return ApiResponse.ok(jobSeekerService.getBookmarks(user));
+    }
+
+    @PostMapping("/{recruitment}")
+    @Operation(summary = "북마크 추가 api")
+    public ApiResponse<Void> addBookmark(@PathVariable Recruitment recruitment, @AuthenticationPrincipal User user) {
+
+        jobSeekerService.addBookmark(recruitment, user);
+        return ApiResponse.ok();
     }
 }
--- src/main/java/com/flex/interpre/domain/user/jobSeeker/service/JobSeekerService.java
+++ src/main/java/com/flex/interpre/domain/user/jobSeeker/service/JobSeekerService.java
@@ -1,5 +1,6 @@
 package com.flex.interpre.domain.user.jobSeeker.service;
 
+import com.flex.interpre.domain.recruitment.entity.Recruitment;
 import com.flex.interpre.domain.user.entity.JobSeeker;
 import com.flex.interpre.domain.user.entity.User;
 import com.flex.interpre.domain.user.exception.UserExceptions;
@@ -7,6 +8,7 @@ import com.flex.interpre.domain.user.jobSeeker.dto.resposne.BookMarkListResponse
 import com.flex.interpre.domain.user.repository.JobSeekerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,4 +24,13 @@ public class JobSeekerService {
         return BookMarkListResponse.from(jobSeeker.getBookmarkedRecruitments());
 
     }
+
+    public void addBookmark(Recruitment recruitment, @AuthenticationPrincipal User user) {
+
+        JobSeeker jobSeeker = jobSeekerRepository.findByIdWithUser(user.getId()).orElseThrow(UserExceptions.USER_NOT_FOUND::toException);
+
+        jobSeeker.getBookmarkedRecruitments().add(recruitment);
+        jobSeekerRepository.save(jobSeeker);
+    }
+
 }
