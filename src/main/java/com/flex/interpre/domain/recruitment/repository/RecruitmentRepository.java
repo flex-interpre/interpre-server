4662e5c974a93f255ab36d289eba4e483e11e9c8
docherryra <docherryra@gmail.com>
2025-10-05 15:54:40 +0900

    [IPR-10] feat(recruitment): 공고문 레포지토리 인터페이스 추가

--- src/main/java/com/flex/interpre/domain/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/flex/interpre/domain/recruitment/repository/RecruitmentRepository.java
@@ -1,4 +1,11 @@
 package com.flex.interpre.domain.recruitment.repository;
 
-public interface RecruitmentRepository {
+import com.flex.interpre.domain.recruitment.entity.Recruitment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RecruitmentRepository extends JpaRepository<Recruitment, UUID> {
 }
