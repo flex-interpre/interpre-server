cdfa06b8c1b5827e819169f52427adbc8ac9b6bd
kimmoongi <moongi0320@naver.com>
2025-10-08 11:41:46 +0900

    [IPR-25] feat(interview): 소켓 통신에서 인증 정보를 담기 위한 redis 객체 생성

---
+++ src/main/java/com/flex/interpre/domain/interview/entity/InterviewSession.java
@@ -0,0 +1,25 @@
+package com.flex.interpre.domain.interview.entity;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Getter
+@Builder
+@RedisHash(value = "interview_session")
+public class InterviewSession {
+
+    @Id
+    @Builder.Default
+    private UUID id = UUID.randomUUID();
+
+    private UUID userId;
+
+    @TimeToLive(unit = TimeUnit.HOURS)
+    long ttl;
+}
