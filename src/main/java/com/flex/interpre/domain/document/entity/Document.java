5c8969cff78768bfbd4a75a2e4bb9f6de496dede
docherryra <docherryra@gmail.com>
2025-10-08 23:56:21 +0900

    [IPR-33] feat(document): document 엔티티 구현

---
+++ src/main/java/com/flex/interpre/domain/document/entity/Document.java
@@ -0,0 +1,55 @@
+package com.flex.interpre.domain.document.entity;
+
+import com.flex.interpre.domain.user.entity.JobSeeker;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name= "documents")
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false, columnDefinition = "uuid")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_seeker_id", nullable = false)
+    private JobSeeker jobSeeker;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "document_type", nullable = false, length = 50)
+    private DocumentType documentType;
+
+    @Column(name = "file_name", nullable = false, length = 255)
+    private String fileName;
+
+    @Column(name = "file_url", nullable = false, length = 500)
+    private String fileUrl;
+
+    @Column(name = "file_size", nullable = false)
+    private int fileSize;
+
+    @Column(columnDefinition = "text", nullable = false)
+    private String contentText;
+
+    @Column(name = "document_vector", columnDefinition = "vector")
+    private List<Double> documentVector;
+}
---
+++ src/main/java/com/flex/interpre/domain/document/entity/DocumentType.java
@@ -0,0 +1,8 @@
+package com.flex.interpre.domain.document.entity;
+
+public enum DocumentType {
+    RESUME,
+    PORTFOLIO,
+    COVER_LETTER,
+    OTHER
+}
