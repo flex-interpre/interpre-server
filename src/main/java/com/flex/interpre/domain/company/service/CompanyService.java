3033012e46933177ade1bcd7fc80230bdca3d075
docherryra <docherryra@gmail.com>
2025-10-10 21:19:06 +0900

    [IPR-36] feat(company): 회사 목록 및 상세 조회 api 구현

--- src/main/java/com/flex/interpre/domain/company/service/CompanyService.java
+++ src/main/java/com/flex/interpre/domain/company/service/CompanyService.java
@@ -1,4 +1,34 @@
 package com.flex.interpre.domain.company.service;
 
+import com.flex.interpre.domain.company.dto.CompanyDetailResponse;
+import com.flex.interpre.domain.company.dto.CompanySummaryResponse;
+import com.flex.interpre.domain.company.entity.Company;
+import com.flex.interpre.domain.recruitment.exception.RecruitmentExceptions;
+import com.flex.interpre.domain.user.repository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public Page<CompanySummaryResponse> getCompanies(Pageable pageable) {
+        Page<Company> companies = companyRepository.findAll(pageable);
+        return companies.map(CompanySummaryResponse::from);
+    }
+
+
+    public CompanyDetailResponse getCompanyDetails(UUID companyId) {
+        Company company = companyRepository.findByIdWithActiveRecruitments(companyId)
+                .orElseThrow(RecruitmentExceptions.COMPANY_NOT_FOUND::toException);
+
+        return CompanyDetailResponse.from(company);
+    }
 }
