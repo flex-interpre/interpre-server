dfb3ebe16026351d5588f3282c0fe00c9509d27d
kimmoongi <moongi0320@naver.com>
2025-10-25 01:22:25 +0900

    [IPR-29] feat(interview): InterviewResponse 필드에 마지막 질문 확인 용 필드 추가

--- src/main/java/com/flex/interpre/domain/interview/dto/response/InterviewResponse.java
+++ src/main/java/com/flex/interpre/domain/interview/dto/response/InterviewResponse.java
@@ -4,9 +4,12 @@ import lombok.Builder;
 
 @Builder
 public record InterviewResponse(
+        // 이전 사용자 응답 stt 한 결과
         String transcription,
+        // 다음 질문
         String question,
         String audio,
-        Integer questionNumber
+        Integer questionNumber,
+        boolean isFinal
 ) {
 }
