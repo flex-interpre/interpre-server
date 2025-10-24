4f42d1e4e171eb220ae1e96ab5028ea316ca3b59
kimmoongi <moongi0320@naver.com>
2025-10-24 15:29:46 +0900

    [IPR-27] feat(interview):interviewSession에 현재 질문 번호 확인 용 필드 추가

--- src/main/java/com/flex/interpre/domain/interview/entity/InterviewSession.java
+++ src/main/java/com/flex/interpre/domain/interview/entity/InterviewSession.java
@@ -2,14 +2,14 @@ package com.flex.interpre.domain.interview.entity;
 
 import jakarta.persistence.Id;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-@Getter
+@Data
 @Builder
 @RedisHash(value = "interview_session")
 public class InterviewSession {
@@ -23,6 +23,9 @@ public class InterviewSession {
     private UUID interviewId;
 
     private String contentText;
+
+    @Builder.Default
+    private Integer currentQuestionNum = 0;
 
     @TimeToLive(unit = TimeUnit.HOURS)
     long ttl;
