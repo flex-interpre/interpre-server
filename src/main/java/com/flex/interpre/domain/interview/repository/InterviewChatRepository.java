d19c4a8fd4b699e069ce448d2731b13f3598ceb0
kimmoongi <moongi0320@naver.com>
2025-10-24 21:48:50 +0900

    [IPR-27] feat(interview): 질문 생성 및 tts로 음성데이터로 변경 후 클라이언트로 전달

--- src/main/java/com/flex/interpre/domain/interview/repository/InterviewChatRepository.java
+++ src/main/java/com/flex/interpre/domain/interview/repository/InterviewChatRepository.java
@@ -3,7 +3,11 @@ package com.flex.interpre.domain.interview.repository;
 import com.flex.interpre.domain.interview.entity.InterviewChat;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InterviewChatRepository extends CrudRepository<InterviewChat, UUID> {
+    InterviewChat findByQuestionNum(Integer questionNum);
+
+    List<InterviewChat> findByInterviewIdOrderByQuestionNum(UUID interviewId);
 }
