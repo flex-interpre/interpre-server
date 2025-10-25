de540b564c3d9b8bdfbc115411c5f483b6a98c51
docherryra <docherryra@gmail.com>
2025-10-25 17:45:02 +0900

    [IPR-39] feat(reqruitment): 공고문 인덱스 문서 클래스 정의

--- src/main/java/com/flex/interpre/domain/recruitment/index/RecruitmentDocumentIndex.java
+++ src/main/java/com/flex/interpre/domain/recruitment/index/RecruitmentDocumentIndex.java
@@ -2,28 +2,32 @@ package com.flex.interpre.domain.recruitment.index;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.opensearch.annotations.Document;
-import org.springframework.data.opensearch.annotations.Field;
-import org.springframework.data.opensearch.annotations.FieldType;
+import java.util.List;
+import java.util.UUID;
 
-@Document(indexName = "recruitment_embeddings")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class RecruitmentDocumentIndex {
 
     @Id
-    private String id;
+    private UUID id;
 
-    @Field(type = FieldType.Text)
     private String title;
-
-    @Field(type = FieldType.Text)
     private String description;
+    private String companyName;
+    private String location;
 
-    // Clova 임베딩 벡터
-    @Field(type = FieldType.Dense_Vector, dims = 1024)
-    private float[] embeddingVector;
+    private List<String> jobAreas;
+    private List<String> jobFirsts;
+    private List<String> jobSeconds;
+    private List<String> jobThirds;
+    private List<String> employmentTypes;
+    private List<String> requirements;
+    private List<String> benefits;
+    private List<String> skills;
+
+    private List<Double> embedding;
 }
