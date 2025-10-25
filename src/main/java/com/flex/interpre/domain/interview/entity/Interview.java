ba6aeb33efa1d78f3e827f63a741069e33e9432c
Moonki Kim <84397561+Kimmoongi0320@users.noreply.github.com>
2025-10-25 23:43:12 +0900

    feat(interview): 인터뷰 기록 저장 시 임베딩 진행 후 저장 (#28)

--- src/main/java/com/flex/interpre/domain/interview/entity/Interview.java
+++ src/main/java/com/flex/interpre/domain/interview/entity/Interview.java
@@ -1,18 +1,27 @@
 package com.flex.interpre.domain.interview.entity;
 
 import com.flex.interpre.domain.user.entity.JobSeeker;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 @Entity
 @Builder
@@ -44,4 +53,7 @@ public class Interview {
     @OneToMany(mappedBy = "interview", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @Builder.Default
     private List<Qna> qnas = new ArrayList<>();
+
+    @Column(name = "embedding")
+    private List<Double> embedding;
 }
