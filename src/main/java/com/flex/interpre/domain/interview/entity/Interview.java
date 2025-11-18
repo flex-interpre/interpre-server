762b57bae7eac63659de2957b82a100cfb004f9b
docherryra <docherryra@gmail.com>
2025-11-18 13:10:43 +0900

    chore(interview): 테이블명 변경

--- src/main/java/com/flex/interpre/domain/interview/entity/Interview.java
+++ src/main/java/com/flex/interpre/domain/interview/entity/Interview.java
@@ -1,18 +1,8 @@
 package com.flex.interpre.domain.interview.entity;
 
 import com.flex.interpre.domain.jobSeeker.entity.JobSeeker;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +20,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @AllArgsConstructor
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
+@Table(name= "interviews")
 public class Interview {
 
     @Id
